b7a26beeb8c1343fe1c64b8b05b58c80afc6cf8c
DaanisTech <135832026+DaanisTech@users.noreply.github.com>
2023-06-11 22:22:31 -0700

    Add files via upload

---
+++ Fahrenheit to celsius converter/Fahrenheit to celsius  converter.py
@@ -0,0 +1,11 @@
+# temp = float(input("Enter temparature in fahrenheit :"))
+
+# celsius =(temp - 32) * 5/9
+
+# print(f"{temp} Fahrenheit is equal to {celsius} in celsius")
+
+celsius = float(input("Enter temparature in celsius :"))
+
+fahrenheit = (celsius + 32) / (5/9)
+
+print(f"{celsius} celsius is equal to {fahrenheit} in fahrenheit")
